cc51708a178db07b7265931bcd6b44fc92b9446d
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2024-04-17 00:46:00 +0100

    Refactor DrumKit script to add TypeScript support

--- DrumKit/script.js
+++ DrumKit/script.js
@@ -2,7 +2,17 @@
 console.log("Hello, World!");
 window.addEventListener("keydown", function (event) {
     const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${event.keyCode}"]`);
     if (!audio)
         return; // stop function from running all together
+    audio.currentTime = 0; // rewind to the start
     audio.play();
+    key.classList.add("playing");
 });
+// alternative for TS
+// window.addEventListener("keydown", function(event) {
+//     const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`) as HTMLAudioElement;
+//     if (audio) {
+//         audio.play();
+//       }
+// });
