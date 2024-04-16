590f219f7a9f32ce10489f4b16f670b88c81e6be
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2024-04-17 00:18:36 +0100

    audio.play added

--- DrumKit/script.js
+++ DrumKit/script.js
@@ -1,2 +1,8 @@
 "use strict";
 console.log("Hello, World!");
+window.addEventListener("keydown", function (event) {
+    const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
+    if (!audio)
+        return; // stop function from running all together
+    audio.play();
+});
--- DrumKit/script.ts
+++ DrumKit/script.ts
@@ -1,1 +1,7 @@
 console.log("Hello, World!");
+
+window.addEventListener("keydown", function(event) {
+    const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`) as HTMLAudioElement;
+    if(!audio) return; // stop function from running all together
+    audio.play();
+});
