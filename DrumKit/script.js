b6f955fa543b07ce9be828a192ce5a03259f4f48
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2024-04-17 01:37:54 +0100

    Refactor DrumKit script to separate event listeners and add TypeScript support

--- DrumKit/script.js
+++ DrumKit/script.js
@@ -1,6 +1,6 @@
 "use strict";
 console.log("Hello, World!");
-window.addEventListener("keydown", function (event) {
+function playSound(event) {
     const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
     const key = document.querySelector(`.key[data-key="${event.keyCode}"]`);
     if (!audio)
@@ -8,7 +8,16 @@ window.addEventListener("keydown", function (event) {
     audio.currentTime = 0; // rewind to the start
     audio.play();
     key.classList.add("playing");
-});
+}
+function removeTransition(event) {
+    if (event.propertyName !== "transform")
+        return; // skip if it's not a transform
+    this.classList.remove("playing");
+}
+const keys = document.querySelectorAll(".key");
+keys.forEach(key => key.addEventListener("transitionend", removeTransition));
+window.addEventListener("keydown", playSound);
+/////////////////////////////////////////////////////////////////
 // alternative for TS
 // window.addEventListener("keydown", function(event) {
 //     const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`) as HTMLAudioElement;
--- DrumKit/script.ts
+++ DrumKit/script.ts
@@ -1,14 +1,24 @@
 console.log("Hello, World!");
 
-window.addEventListener("keydown", function(event) {
-    const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`) as HTMLAudioElement;
-    const key = document.querySelector(`.key[data-key="${event.keyCode}"]`) as HTMLDivElement;
-    if(!audio) return; // stop function from running all together
-    audio.currentTime = 0; // rewind to the start
-    audio.play();
-    key.classList.add("playing");
-});
+function playSound(event: KeyboardEvent) {
+        const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`) as HTMLAudioElement;
+        const key = document.querySelector(`.key[data-key="${event.keyCode}"]`) as HTMLDivElement;
+        if(!audio) return; // stop function from running all together
+        audio.currentTime = 0; // rewind to the start
+        audio.play();
+        key.classList.add("playing");
+}
 
+function removeTransition(event: TransitionEvent) {
+    if (event.propertyName !== "transform") return; // skip if it's not a transform
+    this.classList.remove("playing");
+}
+
+const keys = document.querySelectorAll(".key");
+keys.forEach(key => key.addEventListener("transitionend", removeTransition as EventListener));
+window.addEventListener("keydown", playSound);
+
+/////////////////////////////////////////////////////////////////
 // alternative for TS
 // window.addEventListener("keydown", function(event) {
 //     const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`) as HTMLAudioElement;
@@ -16,4 +26,3 @@ window.addEventListener("keydown", function(event) {
 //         audio.play();
 //       }
 // });
-
