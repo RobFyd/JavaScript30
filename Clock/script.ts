fa1549f978faaf987ee692ecef18a1c3086689c9
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2024-04-18 23:51:23 +0100

    minuteHand and hourHand coded

--- Clock/script.ts
+++ Clock/script.ts
@@ -1,15 +1,21 @@
 const secondHand = document.querySelector('.second-hand') as HTMLDivElement;
-const minuteHand = document.querySelector('.minute-hand') as HTMLDivElement;
+const minuteHand = document.querySelector('.min-hand') as HTMLDivElement;
 const hourHand = document.querySelector('.hour-hand') as HTMLDivElement;
 
 const setDate = () => {
     const now = new Date();
+
     const seconds = now.getSeconds();
     const secondsDegrees = ((seconds / 60) * 360) + 90;
     secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+
     const minutes = now.getMinutes();
+    const minutesDegrees = ((minutes / 60) * 360) + 90;
+    minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
+
     const hours = now.getHours();
-    console.log(hours, minutes, seconds);
+    const hoursDegrees = ((hours / 12) * 360) +90;
+    hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
 };
 
 setInterval(setDate, 1000);
