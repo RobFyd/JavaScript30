ec3df296a2c9771b91a12f0c319b1f1b562da593
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2024-04-18 23:40:05 +0100

    secondHand coded

--- Clock/script.js
+++ Clock/script.js
@@ -1,2 +1,14 @@
 "use strict";
-console.log("Hello, World!");
+const secondHand = document.querySelector('.second-hand');
+const minuteHand = document.querySelector('.minute-hand');
+const hourHand = document.querySelector('.hour-hand');
+const setDate = () => {
+    const now = new Date();
+    const seconds = now.getSeconds();
+    const secondsDegrees = ((seconds / 60) * 360) + 90;
+    secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+    const minutes = now.getMinutes();
+    const hours = now.getHours();
+    console.log(hours, minutes, seconds);
+};
+setInterval(setDate, 1000);
--- Clock/script.ts
+++ Clock/script.ts
@@ -1,1 +1,15 @@
-console.log("Hello, World!");
+const secondHand = document.querySelector('.second-hand') as HTMLDivElement;
+const minuteHand = document.querySelector('.minute-hand') as HTMLDivElement;
+const hourHand = document.querySelector('.hour-hand') as HTMLDivElement;
+
+const setDate = () => {
+    const now = new Date();
+    const seconds = now.getSeconds();
+    const secondsDegrees = ((seconds / 60) * 360) + 90;
+    secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+    const minutes = now.getMinutes();
+    const hours = now.getHours();
+    console.log(hours, minutes, seconds);
+};
+
+setInterval(setDate, 1000);
