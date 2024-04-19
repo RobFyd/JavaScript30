086d013ca9281b8a2653c52b66092e80998f0237
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2024-04-19 23:42:07 +0100

    scripts updated

--- CSSvariables/script.js
+++ CSSvariables/script.js
@@ -1,2 +1,6 @@
-"use strict";
-console.log('CSS variables script loaded');
+const inputs = document.querySelectorAll('.controls input');
+function handleUpdate() {
+    console.log(this.value);
+}
+inputs.forEach(input => input.addEventListener('change', handleUpdate));
+inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
--- CSSvariables/script.ts
+++ CSSvariables/script.ts
@@ -1,1 +1,8 @@
-console.log('CSS variables script loaded');
+const inputs = document.querySelectorAll('.controls input') as NodeListOf<HTMLInputElement>;
+
+function handleUpdate() {
+    console.log(this.value);
+}
+
+inputs.forEach(input => input.addEventListener('change', handleUpdate));
+inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
