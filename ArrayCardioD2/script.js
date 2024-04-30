71de0ec9740ca191c54800d85337608d2ea5abab
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2024-05-01 00:17:00 +0100

    some method continued

--- ArrayCardioD2/script.js
+++ ArrayCardioD2/script.js
@@ -1,27 +1,33 @@
-console.log('Array Cardio Day 2');
+console.log("Array Cardio Day 2");
 // ## Array Cardio Day 2
-const people = [
-    { name: 'Wes', year: 1988 },
-    { name: 'Kait', year: 1986 },
-    { name: 'Irv', year: 1970 },
-    { name: 'Lux', year: 2015 }
+const people2 = [
+    { name: "Wes", year: 1988 },
+    { name: "Kait", year: 1986 },
+    { name: "Irv", year: 1970 },
+    { name: "Lux", year: 2015 },
 ];
 const comments = [
-    { text: 'Love this!', id: 523423 },
-    { text: 'Super good', id: 823423 },
-    { text: 'You are the best', id: 2039842 },
-    { text: 'Ramen is my fav food ever', id: 123523 },
-    { text: 'Nice Nice Nice!', id: 542328 }
+    { text: "Love this!", id: 523423 },
+    { text: "Super good", id: 823423 },
+    { text: "You are the best", id: 2039842 },
+    { text: "Ramen is my fav food ever", id: 123523 },
+    { text: "Nice Nice Nice!", id: 542328 },
 ];
 // Some and Every Checks
 // Array.prototype.some() // is at least one person 19 or older?
-const isAdult = people.some(function (person) {
-    const currentYear = (new Date()).getFullYear();
+const isAdult = people2.some(function (person) {
+    const currentYear = new Date().getFullYear();
     if (currentYear - person.year >= 19) {
         return true;
     }
 });
-console.log(isAdult);
+// short version
+const isAdult2 = people2.some((person) => {
+    const currentYear = new Date().getFullYear();
+    return currentYear - person.year >= 19;
+});
+console.log({ isAdult }); // {} - to show the result as an object
+console.log(isAdult2);
 // Array.prototype.every() // is everyone 19 or older?
 // Array.prototype.find()
 // Find is like filter, but instead returns just the one you are looking for
