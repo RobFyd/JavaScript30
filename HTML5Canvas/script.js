8348899b58e3a6cc532468165a6dfecfedaac43b
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2024-05-02 00:32:19 +0100

    Refactor HTML5 Canvas script to enable drawing functionality

--- HTML5Canvas/script.js
+++ HTML5Canvas/script.js
@@ -2,9 +2,10 @@ const canvas = document.querySelector('#draw');
 const ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
-ctx.strokeStyle = '#BADA55';
+ctx.strokeStyle = 'black';
 ctx.lineJoin = 'round';
 ctx.lineCap = 'round';
+ctx.lineWidth = 100;
 let isDrawing = false;
 let lastX = 0;
 let lastY = 0;
@@ -12,8 +13,21 @@ function draw(e) {
     if (!isDrawing)
         return; // stop the fn from running when they are not moused down
     console.log(e);
+    // Add your drawing logic here
+    ctx.beginPath();
+    // start from
+    ctx.moveTo(lastX, lastY);
+    // go to
+    ctx.lineTo(e.offsetX, e.offsetY);
+    ctx.stroke();
+    // lastX = e.offsetX;
+    // lastY = e.offsetY;
+    [lastX, lastY] = [e.offsetX, e.offsetY]; // ES6 destructuring assignment syntax to set lastX and lastY to the current mouse position
 }
+canvas.addEventListener('mousedown', (e) => {
+    isDrawing = true;
+    [lastX, lastY] = [e.offsetX, e.offsetY];
+});
 canvas.addEventListener('mousemove', draw);
-canvas.addEventListener('mousedown', () => isDrawing = true);
 canvas.addEventListener('mouseup', () => isDrawing = false);
 canvas.addEventListener('mouseout', () => isDrawing = false);
--- HTML5Canvas/script.ts
+++ HTML5Canvas/script.ts
@@ -3,20 +3,33 @@ const ctx = canvas.getContext('2d') as CanvasRenderingContext2D;
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
-ctx.strokeStyle = '#BADA55';
+ctx.strokeStyle = 'black';
 ctx.lineJoin = 'round';
 ctx.lineCap = 'round';
+ctx.lineWidth = 100;
 
 let isDrawing = false;
 let lastX = 0;
 let lastY = 0;
 
-function draw(e) {
+function draw(e: MouseEvent): void {
     if (!isDrawing) return; // stop the fn from running when they are not moused down
     console.log(e);
+    // Add your drawing logic here
+    ctx.beginPath();
+    // start from
+    ctx.moveTo(lastX, lastY);
+    // go to
+    ctx.lineTo(e.offsetX, e.offsetY);
+    ctx.stroke();
+    // lastX = e.offsetX;
+    // lastY = e.offsetY;
+    [lastX, lastY] = [e.offsetX, e.offsetY]; // ES6 destructuring assignment syntax to set lastX and lastY to the current mouse position
 }
-
+canvas.addEventListener('mousedown', (e) => {
+    isDrawing = true;
+    [lastX, lastY] = [e.offsetX, e.offsetY];
+});
 canvas.addEventListener('mousemove', draw);
-canvas.addEventListener('mousedown', () => isDrawing = true);
 canvas.addEventListener('mouseup', () => isDrawing = false);
 canvas.addEventListener('mouseout', () => isDrawing = false);
