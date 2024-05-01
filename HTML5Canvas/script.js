9203fdfb927c80352079010f4d921deb56290ddc
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2024-05-02 00:51:58 +0100

    Update HTML5 Canvas script to adjust line width and color dynamically

--- HTML5Canvas/script.js
+++ HTML5Canvas/script.js
@@ -6,14 +6,18 @@ ctx.strokeStyle = 'black';
 ctx.lineJoin = 'round';
 ctx.lineCap = 'round';
 ctx.lineWidth = 100;
+// ctx.globalCompositeOperation = 'multiply'
 let isDrawing = false;
 let lastX = 0;
 let lastY = 0;
+let hue = 0;
+let direction = true;
 function draw(e) {
     if (!isDrawing)
         return; // stop the fn from running when they are not moused down
     console.log(e);
     // Add your drawing logic here
+    ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
     ctx.beginPath();
     // start from
     ctx.moveTo(lastX, lastY);
@@ -23,6 +27,19 @@ function draw(e) {
     // lastX = e.offsetX;
     // lastY = e.offsetY;
     [lastX, lastY] = [e.offsetX, e.offsetY]; // ES6 destructuring assignment syntax to set lastX and lastY to the current mouse position
+    hue++;
+    if (hue >= 360) {
+        hue = 0;
+    }
+    if (ctx.lineWidth >= 100 || ctx.lineWidth <= 1) {
+        direction = !direction;
+    }
+    if (direction) {
+        ctx.lineWidth++;
+    }
+    else {
+        ctx.lineWidth--;
+    }
 }
 canvas.addEventListener('mousedown', (e) => {
     isDrawing = true;
