c51380a5beb7a2a28d4a647ec308fdfdd56223ef
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2024-05-01 01:16:03 +0100

    find method added

--- ArrayCardioD2/script.js
+++ ArrayCardioD2/script.js
@@ -37,6 +37,15 @@ console.log(isEveryAdult);
 // Array.prototype.find()
 // Find is like filter, but instead returns just the one you are looking for
 // find the comment with the ID of 823423
+const rightComment = comments.find(function (comment) {
+    if (comment.id === 823423) {
+        return true;
+    }
+});
+console.log({ rightComment });
+// simpler
+const rightComment2 = comments.find((comment) => comment.id === 823423);
+console.log(rightComment2);
 // Array.prototype.findIndex()
 // Find the comment with this ID
 // delete the comment with the ID of 823423
--- ArrayCardioD2/script.ts
+++ ArrayCardioD2/script.ts
@@ -36,21 +36,29 @@ const isAdult2 = people2.some((person) => {
 const isAdult3 = people2.some(
   (person) => new Date().getFullYear() - person.year >= 19
 );
-
 console.log({ isAdult }); // {} - to show the result as an object
 console.log(isAdult2);
 console.log(isAdult3);
 
 // Array.prototype.every() // is everyone 19 or older?
 const isEveryAdult = people2.every(
-    (person) => new Date().getFullYear() - person.year >= 19
-)
-
+  (person) => new Date().getFullYear() - person.year >= 19
+);
 console.log(isEveryAdult);
 
 // Array.prototype.find()
 // Find is like filter, but instead returns just the one you are looking for
 // find the comment with the ID of 823423
+const rightComment = comments.find(function(comment) {
+    if(comment.id === 823423) {
+        return true;
+    }
+});
+console.log({rightComment});
+
+// simpler
+const rightComment2 = comments.find((comment) => comment.id === 823423);
+console.log(rightComment2);
 
 // Array.prototype.findIndex()
 // Find the comment with this ID
