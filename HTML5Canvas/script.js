bcacdb463fa5bbc9a5ad46d30fcfd750e512d38e
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2024-05-01 23:50:39 +0100

    Add HTML5 Canvas script file

--- HTML5Canvas/script.js
+++ HTML5Canvas/script.js
@@ -1,18 +1,14 @@
-const canvas = document.querySelector('#draw') as HTMLCanvasElement;
-const ctx = canvas.getContext('2d') as CanvasRenderingContext2D;
-
+const canvas = document.querySelector('#draw');
+const ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 ctx.strokeStyle = '#BADA55';
 ctx.lineJoin = 'round';
 ctx.lineCap = 'round';
-
 let isDrawing = false;
 let lastX = 0;
 let lastY = 0;
-
 function draw(e) {
     console.log(e);
 }
-
 canvas.addEventListener('mousemove', draw);
