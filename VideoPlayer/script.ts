37f8e27f0803449759d3dbdd13137f107d422b15
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2024-05-04 23:29:09 +0100

    function togglePlay, updateButton added

---
+++ VideoPlayer/script.ts
@@ -0,0 +1,36 @@
+// get our elements
+
+const player = document.querySelector('.player') as HTMLDivElement;
+const video = player.querySelector('.viewer') as HTMLVideoElement;
+const progress = player.querySelector('.progress') as HTMLDivElement;
+const progressBar = player.querySelector('.progress__filled') as HTMLDivElement;
+const toggle = player.querySelector('.toggle') as HTMLButtonElement;
+const skipButtons = player.querySelectorAll('[data-skip]') as NodeListOf<HTMLButtonElement>;
+const ranges = player.querySelectorAll('.player__slider') as NodeListOf<HTMLInputElement>;
+
+// build out functions
+
+function togglePlay() {
+    const method = video.paused ? 'play' : 'pause';
+    video[method]();
+
+    // if(video.paused) {
+    //     video.play();
+    // } else {
+    //     video.pause();
+    // }
+    console.log(method);
+}
+
+function updateButton() {
+    const icon = this.paused ? '►' : '❚ ❚';
+    toggle.textContent = icon;
+    console.log(icon);
+}
+
+// hook up the event listeners
+
+video.addEventListener('click', togglePlay);
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
+toggle.addEventListener('click', togglePlay);
