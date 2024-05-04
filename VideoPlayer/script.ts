a9de042552ddaf470c5b8ac6d6fb2f5facccb2b1
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2024-05-05 00:32:30 +0100

    progress coded

--- VideoPlayer/script.ts
+++ VideoPlayer/script.ts
@@ -39,13 +39,27 @@ function handleRangeUpdate() {
     console.log(this.value);
 }
 
+function handleProgress() {
+    const percent = (video.currentTime / video.duration) * 100;
+    progressBar.style.flexBasis = `${percent}%`;
+}
+
+function scrub(e) {
+    const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+    console.log(e);
+    video.currentTime = scrubTime;
+}
+
 // hook up the event listeners
 
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', updateButton);
 video.addEventListener('pause', updateButton);
+video.addEventListener('timeupdate', handleProgress);
 
 toggle.addEventListener('click', togglePlay);
 skipButtons.forEach(button => button.addEventListener('click', skip));
 ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
 ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate));
+
+progress.addEventListener('click', scrub);
