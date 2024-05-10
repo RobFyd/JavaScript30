3bd103f85e376e44cc5d81a81d6a4f7c85155032
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2024-05-10 01:07:58 +0100

    refactor: Update LocalStorage script to prevent form submission and add item to items array

--- LocalStorage/script.js
+++ LocalStorage/script.js
@@ -2,6 +2,13 @@ const addItems = document.querySelector(".add-items");
 const itemsList = document.querySelector(".plates");
 const items = [];
 function addItem(e) {
-    console.log("Hello");
+    e.preventDefault();
+    // const text = (document.querySelector("[name=item]")).value;
+    const text = (this.querySelector("[name=item]")).value;
+    const item = {
+        text,
+        done: false,
+    };
+    console.log(item);
 }
 addItems.addEventListener("submit", addItem);
--- LocalStorage/script.ts
+++ LocalStorage/script.ts
@@ -2,8 +2,15 @@ const addItems = document.querySelector(".add-items");
 const itemsList = document.querySelector(".plates");
 const items = [];
 
-function addItem(e) {
-    console.log("Hello");
+function addItem(e: Event) {
+    e.preventDefault();
+    // const text = (document.querySelector("[name=item]")).value;
+    const text = (this.querySelector("[name=item]")).value;
+    const item = {
+        text,
+        done: false,
+    };
+    console.log(item);
 }
 
 addItems.addEventListener("submit", addItem);
