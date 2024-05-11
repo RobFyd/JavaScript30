5a8f4940f269011fe6519056160fa1830dedfd54
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2024-05-11 22:53:44 +0100

    Add lazy loading for images in MouseMoveShadow project

--- MouseMoveShadow/script.ts
+++ MouseMoveShadow/script.ts
@@ -2,7 +2,7 @@ const hero: HTMLDivElement = document.querySelector('.hero');
 const text = hero.querySelector('h1');
 
 function shadow(e: MouseEvent) {
-
+    console.log(e);
 }
 
 hero.addEventListener('mousemove', shadow);
