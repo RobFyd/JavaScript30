3bdf1ff55717673ee3fda128e793b78cec28affe
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2024-05-11 23:14:49 +0100

    Update MouseMoveShadow script to improve performance and readability

--- MouseMoveShadow/script.js
+++ MouseMoveShadow/script.js
@@ -1,6 +1,14 @@
-const hero = document.querySelector('.hero');
-const text = hero.querySelector('h1');
+const hero = document.querySelector(".hero");
+const text = hero.querySelector("h1");
 function shadow(e) {
-    console.log(e);
+    const width = hero.offsetWidth;
+    const height = hero.offsetHeight;
+    //     const { offsetWidth: width, offsetHeight: height } = hero; // Destructuring but same as above
+    let { offsetX: x, offsetY: y } = e;
+    if (this !== e.target) {
+        x = x + e.target.offsetLeft;
+        y = y + e.target.offsetTop;
+    }
+    console.log(x, y);
 }
-hero.addEventListener('mousemove', shadow);
+hero.addEventListener("mousemove", shadow);
--- MouseMoveShadow/script.ts
+++ MouseMoveShadow/script.ts
@@ -1,8 +1,18 @@
-const hero: HTMLDivElement = document.querySelector('.hero');
-const text = hero.querySelector('h1');
+const hero: HTMLDivElement = document.querySelector(".hero");
+const text = hero.querySelector("h1");
 
 function shadow(e: MouseEvent) {
-    console.log(e);
+  const width = hero.offsetWidth;
+  const height = hero.offsetHeight;
+  //     const { offsetWidth: width, offsetHeight: height } = hero; // Destructuring but same as above
+  let { offsetX: x, offsetY: y } = e;
+
+  if (this !== e.target) {
+    x = x + e.target.offsetLeft;
+    y = y + e.target.offsetTop;
+  }
+
+  console.log(x, y);
 }
 
-hero.addEventListener('mousemove', shadow);
+hero.addEventListener("mousemove", shadow);
