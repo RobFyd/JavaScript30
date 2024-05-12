4e54bd85624069f7593dbe157a81cb180589a8d1
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2024-05-12 23:43:56 +0100

    ternary operator and arrow function added

--- SortWithoutArticles/script.ts
+++ SortWithoutArticles/script.ts
@@ -18,12 +18,26 @@ function strip(bandName: string) {
   return bandName.replace(/^(a |the |an )/i, "").trim();
 }
 
-const sortedBands = bands.sort(function (a, b) {
-  if (a > b) {
-    return 1;
-  } else {
-    return -1;
-  }
-});
+// const sortedBands = bands.sort(function (a, b) {   // before stripping
+//   if (a > b) {
+//     return 1;
+//   } else {
+//     return -1;
+//   }
+// });
+
+// const sortedBands = bands.sort(function (a, b) {   // after stripping
+//   if (strip(a) > strip(b)) {
+//     return 1;
+//   } else {
+//     return -1;
+//   }
+// });
+
+// const sortedBands = bands.sort(function (a, b) {    // after stripping and using ternary operator
+//   return strip(a) > strip(b) ? 1 : -1;
+// });
+
+const sortedBands = bands.sort((a, b) => strip(a) > strip(b) ? 1 : -1);   // after stripping and using ternary operator and arrow function
 
 console.log(sortedBands);
