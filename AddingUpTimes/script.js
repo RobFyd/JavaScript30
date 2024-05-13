4232f7777defcc5acd3790445965b6b5163ba699
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2024-05-14 00:29:47 +0100

    Refactor AddingUpTimes script to use Array.from and console.table

--- AddingUpTimes/script.js
+++ AddingUpTimes/script.js
@@ -10,5 +10,7 @@ const seconds = timeNodes
 let secondsLeft = seconds;
 const hours = Math.floor(secondsLeft / 3600);
 secondsLeft = secondsLeft % 3600;
+const mins = Math.floor(secondsLeft / 60);
+secondsLeft = secondsLeft % 60;
 console.log(seconds);
-console.log(secondsLeft);
+console.log(hours, mins, secondsLeft);
--- AddingUpTimes/script.ts
+++ AddingUpTimes/script.ts
@@ -14,5 +14,8 @@ const seconds = timeNodes
     const hours = Math.floor(secondsLeft / 3600);
     secondsLeft = secondsLeft % 3600;
 
+    const mins = Math.floor(secondsLeft / 60);
+    secondsLeft = secondsLeft % 60;
+
 console.log(seconds);
-console.log(secondsLeft);
+console.log(hours, mins, secondsLeft);
