2ff5248e10c5d450cc2713f6cc05ea5613182920
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2024-05-13 23:57:32 +0100

    Refactor AddingUpTimes script to use Array.from and console.table

--- AddingUpTimes/script.ts
+++ AddingUpTimes/script.ts
@@ -2,6 +2,12 @@ const timeNodes = Array.from(document.querySelectorAll('[data-time]'));
 
 console.log(timeNodes);
 
-const seconds = timeNodes.map(node => node.dataset.time);
+const seconds = timeNodes
+    .map(node => node.dataset.time)
+    .map(timeCode => {
+        const [mins, secs] = timeCode.split(':').map(parseFloat);
+        return (mins * 60) + secs;
+        console.log(mins, secs);
+    })
 
 console.table(seconds);
