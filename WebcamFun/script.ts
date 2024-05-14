25fa3f11728e7c7ba1138aca21148bfe85b096c3
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2024-05-15 00:31:19 +0100

    Refactor script.js to use TypeScript and update index.html to reflect the changes

--- WebcamFun/script.ts
+++ WebcamFun/script.ts
@@ -1,5 +1,16 @@
 const video = document.querySelector(".player") as HTMLVideoElement;
 const canvas = document.querySelector(".photo") as HTMLCanvasElement;
-// const ctx = canvas.getContext('2d') as CanvasRenderingContext2D;
+const ctx = canvas.getContext('2d') as CanvasRenderingContext2D;
 const strip = document.querySelector(".strip") as HTMLDivElement;
 const snap = document.querySelector(".snap") as HTMLAudioElement;
+
+
+function getVideo() {
+    navigator.mediaDevices.getUserMedia({ video: true, audio: false })
+        .then(localMediaStream => {
+            console.log(localMediaStream);
+            video.src = window.URL.createObjectURL(localMediaStream);
+        });
+}
+
+getVideo();
