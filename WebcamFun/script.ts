748843d8eee1826c48609aac7d21ec04babb3d99
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2024-05-15 23:44:00 +0100

    Refactor script.js to use TypeScript and add takePhoto function

--- WebcamFun/script.ts
+++ WebcamFun/script.ts
@@ -29,6 +29,12 @@ function paintToCanvas() {
     }, 16);  
         
 }
-    
 
+function takePhoto() {
+    snap.currentTime = 0;
+    snap.play();
+}
+    
 getVideo();
+
+video.addEventListener('canplay', paintToCanvas);
