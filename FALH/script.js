5dc6be76e593990527749f4bca5a12acf15a9fc0
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2024-05-18 21:41:52 +0100

    Refactor highlightLink function to dynamically set the width and height of the highlight element

--- FALH/script.js
+++ FALH/script.js
@@ -3,6 +3,9 @@ const highlight = document.createElement('span');
 highlight.classList.add('highlight');
 document.body.append(highlight);
 function highlightLink() {
-    console.log(this);
+    const linkCoords = this.getBoundingClientRect();
+    console.log(linkCoords);
+    highlight.style.width = `${linkCoords.width}px`;
+    highlight.style.height = `${linkCoords.height}px`;
 }
 triggers.forEach(a => a.addEventListener('mouseenter', highlightLink));
--- FALH/script.ts
+++ FALH/script.ts
@@ -4,7 +4,10 @@ highlight.classList.add('highlight');
 document.body.append(highlight);
 
 function highlightLink() {
-    console.log(this);
+    const linkCoords = this.getBoundingClientRect();
+    console.log(linkCoords);
+    highlight.style.width = `${linkCoords.width}px`;
+    highlight.style.height = `${linkCoords.height}px`;
 }
 
 triggers.forEach(a => a.addEventListener('mouseenter', highlightLink));
