4343ec20aa075fbc9264392d66b3dd9073a3335e
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2024-05-18 21:37:58 +0100

    Update script file name and add event listener for mouseenter event

---
+++ FALH/script.js
@@ -0,0 +1,8 @@
+const triggers = document.querySelectorAll('a');
+const highlight = document.createElement('span');
+highlight.classList.add('highlight');
+document.body.append(highlight);
+function highlightLink() {
+    console.log(this);
+}
+triggers.forEach(a => a.addEventListener('mouseenter', highlightLink));
