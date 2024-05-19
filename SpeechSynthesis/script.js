d55adca213c87f0410852330615946ee67ebaad8
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2024-05-19 22:47:57 +0100

    Refactor speech synthesis script to dynamically populate voices

--- SpeechSynthesis/script.js
+++ SpeechSynthesis/script.js
@@ -7,10 +7,10 @@ const stopButton = document.querySelector("#stop");
 msg.text = document.querySelector('[name="text"]').value;
 function populateVoices() {
     voices = this.getVoices();
-    const voiceOptions = voices
+    voicesDropdown.innerHTML = voices
+        .filter(voice => voice.lang.includes("en"))
         .map(voice => `<option value="${voice.name}">${voice.name} (${voice.lang})</option>`)
         .join("");
-    voicesDropdown.innerHTML = voiceOptions;
 }
 function setVoice() {
     msg.voice = voices.find(voice => voice.name === this.value);
@@ -22,5 +22,13 @@ function toggle(startOver = true) {
         speechSynthesis.speak(msg);
     }
 }
+function setOption() {
+    console.log(this.name, this.value);
+    msg[this.name] = this.value;
+    toggle();
+}
 speechSynthesis.addEventListener("voiceschanged", populateVoices);
 voicesDropdown.addEventListener("change", setVoice);
+options.forEach(option => option.addEventListener("change", setOption));
+speakButton.addEventListener("click", toggle);
+stopButton.addEventListener("click", () => toggle(false));
