c840e30e9abadbf58e284957757307c128dd9102
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2024-05-20 23:47:31 +0100

    Update event capture script to use TypeScript

--- EventCapture/script.js
+++ EventCapture/script.js
@@ -1,1 +1,11 @@
 console.log('Hello, world!');
+const divs = document.querySelectorAll('div');
+function logText(e) {
+    console.log(this.classList.value);
+    e.stopPropagation(); // stop bubbling!
+    // console.log(this);
+}
+document.body.addEventListener('click', logText);
+divs.forEach(div => div.addEventListener('click', logText, {
+    capture: false,
+}));
--- EventCapture/script.ts
+++ EventCapture/script.ts
@@ -1,1 +1,16 @@
 console.log('Hello, world!');
+
+const divs = document.querySelectorAll('div') as NodeListOf<HTMLDivElement>;
+
+function logText(e) {
+    console.log(this.classList.value);
+    e.stopPropagation(); // stop bubbling!
+    // console.log(this);
+}
+
+document.body.addEventListener('click', logText);
+
+divs.forEach(div => div.addEventListener('click', logText, {
+    capture: false,
+    
+}));
