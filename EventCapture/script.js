1271e47dfc1ec91a8457211a675984dff5d7f092
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2024-05-20 23:52:55 +0100

    Refactor event capture script to improve code readability and maintainability

--- EventCapture/script.js
+++ EventCapture/script.js
@@ -2,10 +2,11 @@ console.log('Hello, world!');
 const divs = document.querySelectorAll('div');
 function logText(e) {
     console.log(this.classList.value);
-    e.stopPropagation(); // stop bubbling!
+    // e.stopPropagation(); // stop bubbling!
     // console.log(this);
 }
 document.body.addEventListener('click', logText);
 divs.forEach(div => div.addEventListener('click', logText, {
-    capture: false,
+    capture: false, // if true, the event will be captured in the capture phase
+    once: true // if true, the event will be triggered only once
 }));
--- EventCapture/script.ts
+++ EventCapture/script.ts
@@ -4,13 +4,13 @@ const divs = document.querySelectorAll('div') as NodeListOf<HTMLDivElement>;
 
 function logText(e) {
     console.log(this.classList.value);
-    e.stopPropagation(); // stop bubbling!
+    // e.stopPropagation(); // stop bubbling!
     // console.log(this);
 }
 
 document.body.addEventListener('click', logText);
 
 divs.forEach(div => div.addEventListener('click', logText, {
-    capture: false,
-    
+    capture: false, // if true, the event will be captured in the capture phase
+    once: true // if true, the event will be triggered only once
 }));
