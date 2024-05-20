61e1c787a7b5e310f7b690c5c4a040dc3d15252c
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2024-05-20 23:58:56 +0100

    once added

--- EventCapture/script.js
+++ EventCapture/script.js
@@ -1,12 +1,18 @@
 console.log('Hello, world!');
 const divs = document.querySelectorAll('div');
+const button = document.querySelector('button');
 function logText(e) {
     console.log(this.classList.value);
-    // e.stopPropagation(); // stop bubbling!
+    e.stopPropagation(); // stop bubbling!
     // console.log(this);
 }
 document.body.addEventListener('click', logText);
 divs.forEach(div => div.addEventListener('click', logText, {
     capture: false, // if true, the event will be captured in the capture phase
-    once: true // if true, the event will be triggered only once
+    // once: true // if true, the event will be triggered only once
 }));
+button.addEventListener('click', () => {
+    console.log('Click!!!');
+}, {
+    once: true // if true, the event will be triggered only once
+});
