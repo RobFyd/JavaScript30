5de4509ef8b3b3b16212930698330494edd04a04
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2024-05-21 23:39:54 +0100

    Refactor event capture script to use TypeScript and improve code readability

--- SFAD/script.ts
+++ SFAD/script.ts
@@ -1,1 +1,16 @@
 console.log('Hello, World!');
+
+const triggers = document.querySelectorAll('.cool > li') as NodeListOf<HTMLLIElement>;
+const background = document.querySelector('.dropdownBackground') as HTMLDivElement;
+const nav = document.querySelector('.top') as HTMLElement;
+
+function handleEnter() {
+
+}
+
+function handleLeave() {
+
+}
+
+triggers.forEach(trigger => trigger.addEventListener('mouseenter', handleEnter));
+triggers.forEach(trigger => trigger.addEventListener('mouseleave', handleLeave));
