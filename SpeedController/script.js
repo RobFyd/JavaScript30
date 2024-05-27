283efc7882d4065d4599511ff4750190e367f5b8
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2024-05-28 00:05:01 +0100

    Refactor event capture script to use TypeScript and improve code readability

--- SpeedController/script.js
+++ SpeedController/script.js
@@ -3,4 +3,16 @@ console.log("Hello, world!");
     const speed = document.querySelector(".speed");
     const bar = speed.querySelector(".speed-bar");
     const video = document.querySelector(".flex");
+    function handleMove(event) {
+        const y = event.pageY - speed.offsetTop;
+        const percent = y / speed.offsetHeight;
+        const min = 0.4;
+        const max = 4;
+        const height = Math.round(percent * 100) + "%";
+        const playbackRate = percent * (max - min) + min;
+        bar.style.height = height;
+        bar.textContent = playbackRate.toFixed(2) + "x";
+        video.playbackRate = playbackRate;
+    }
+    speed.addEventListener("mousemove", handleMove);
 }
--- SpeedController/script.ts
+++ SpeedController/script.ts
@@ -4,4 +4,18 @@ console.log("Hello, world!");
   const speed = document.querySelector(".speed") as HTMLDivElement;
   const bar = speed.querySelector(".speed-bar") as HTMLDivElement;
   const video = document.querySelector(".flex") as HTMLVideoElement;
+
+  function handleMove(event: MouseEvent) {
+    const y = event.pageY - speed.offsetTop;
+    const percent = y / speed.offsetHeight;
+    const min = 0.4;
+    const max = 4;
+    const height = Math.round(percent * 100) + "%";
+    const playbackRate = percent * (max - min) + min;
+    bar.style.height = height;
+    bar.textContent = playbackRate.toFixed(2) + "x";
+    video.playbackRate = playbackRate;
+  }
+
+  speed.addEventListener("mousemove", handleMove);
 }
