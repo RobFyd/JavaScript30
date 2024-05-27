02b23094b755db4f758524f2c708f1e3ae21ee1c
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2024-05-27 22:42:13 +0100

    Refactor event capture script to use TypeScript and improve code readability

--- ClickAndDrag/script.ts
+++ ClickAndDrag/script.ts
@@ -7,18 +7,22 @@ let scrollLeft: number;
 
 slider.addEventListener("mousedown", () => {
     isDown = true;
+    slider.classList.add("active");
 });
 
 slider.addEventListener("mouseleave", () => {
     isDown = false;
+    slider.classList.remove("active");
 });
 
 slider.addEventListener("mouseup", () => {
     isDown = false;
+    slider.classList.remove("active");
 });
 
 slider.addEventListener("mousemove", () => {
-    console.log(isDown);
-    console.log("mousemove");
+    if (!isDown) return; // stop the fn from running
+    console.count(isDown);
+    
 });
 
